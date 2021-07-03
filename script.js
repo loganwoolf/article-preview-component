df9b94201c6987b9a453546d20b1ec3774fdaac5
Logan Woolf <64603789+loganwoolf@users.noreply.github.com>
2021-07-03 00:37:24 -0600

    style and implement showSharePanel

--- script.js
+++ script.js
@@ -3,17 +3,20 @@ const avatar = document.querySelector('.avatar');
 const shareLabel = document.querySelector('.share');
 const postInfo = document.querySelector('.post-info');
 const socialButtons = document.querySelector('.social');
-
+const arrow = document.querySelector('.arrow');
 const darkColor = 'hsl(217, 19%, 35%)';
 
 const shareButton = document.querySelector('.share-button');
 
-function toggleSharePanel() {
+function showSharePanel() {
   if (!shareButton.classList.contains('dark')) {
     //darken background of authorPanel
     authorPanel.classList.add('dark');
     //switch to dark button;
-
+      //background of button
+    shareButton.classList.add('medium');
+      //arrow on button
+    arrow.classList.add('brighten');
     //hide avatar
     avatar.classList.add('hidden');
     //hide postInfo
@@ -27,4 +30,4 @@ function toggleSharePanel() {
   }
 
 }
-shareButton.addEventListener('click', toggleSharePanel);
+shareButton.addEventListener('click', showSharePanel);
