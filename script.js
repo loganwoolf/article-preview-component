b2f8ecfe390fad44126e988dfec4eff6dfa2a563
Logan Woolf <64603789+loganwoolf@users.noreply.github.com>
2021-07-02 23:47:29 -0600

    link script to index, create variables for dynamic elements

---
+++ script.js
@@ -0,0 +1,13 @@
+const authorPanel = document.querySelector('.author-panel');
+const avatar = document.querySelector('.avatar');
+const shareLabel = document.querySelector('.share');
+const postInfo = document.querySelector('.post-info');
+const socialButtons = document.querySelector('.social');
+
+const shareButton = document.querySelector('.share-button');
+
+function toggleSharePanel() {
+console.log('click');
+
+}
+shareButton.addEventListener('click', toggleSharePanel);
