2cd50289f103f6a9176c29996ca9d94660a50c3c
Logan Woolf <64603789+loganwoolf@users.noreply.github.com>
2021-07-03 00:15:19 -0600

    add numerous class switchers to share button

--- script.js
+++ script.js
@@ -4,10 +4,27 @@ const shareLabel = document.querySelector('.share');
 const postInfo = document.querySelector('.post-info');
 const socialButtons = document.querySelector('.social');
 
+const darkColor = 'hsl(217, 19%, 35%)';
+
 const shareButton = document.querySelector('.share-button');
 
 function toggleSharePanel() {
-console.log('click');
+  if (!shareButton.classList.contains('dark')) {
+    //darken background of authorPanel
+    authorPanel.classList.add('dark');
+    //switch to dark button;
+
+    //hide avatar
+    avatar.classList.add('hidden');
+    //hide postInfo
+    postInfo.classList.add('hidden');
+    //unhide shareLabel
+    shareLabel.classList.remove('hidden');
+    //unhide socialButtons
+    socialButtons.classList.remove('hidden');
+
+
+  }
 
 }
 shareButton.addEventListener('click', toggleSharePanel);
